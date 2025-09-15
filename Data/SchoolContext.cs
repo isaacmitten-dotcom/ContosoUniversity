45a07165b1278175067e30afc8fc9cd8ef947875
isaac <isaac@DESKTOP-SQKSS0J>
2025-09-15 10:57:28 -0400

    Add project files.

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+
+        public DbSet<Course> Course { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+
+
+        }
+
+
+    }
+}
